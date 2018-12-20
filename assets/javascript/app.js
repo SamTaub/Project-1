1ff125799343b3b5197045427e10c53a490aa740
SamTaub <43358212+SamTaub@users.noreply.github.com>
2018-12-19 20:22:51 -0500

    added firebase api (#2)

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAbF3nKAaCZqR4M16UtQapLikADKBnGsPs",
+    authDomain: "upenn-project-1-2f463.firebaseapp.com",
+    databaseURL: "https://upenn-project-1-2f463.firebaseio.com",
+    projectId: "upenn-project-1-2f463",
+    storageBucket: "upenn-project-1-2f463.appspot.com",
+    messagingSenderId: "43611725747"
+  };
+  firebase.initializeApp(config);
