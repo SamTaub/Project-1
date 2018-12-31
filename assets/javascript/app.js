c84db1cc5de3b4c16369f0e0acac8627831c568e
Ronald Glover <ron881212@gmail.com>
2018-12-31 01:24:20 -0500

    added fully functionality to team logos

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,59 +1,72 @@
-  // Initialize Firebase
-  // var config = {
-  //   apiKey: "AIzaSyAbF3nKAaCZqR4M16UtQapLikADKBnGsPs",
-  //   authDomain: "upenn-project-1-2f463.firebaseapp.com",
-  //   databaseURL: "https://upenn-project-1-2f463.firebaseio.com",
-  //   projectId: "upenn-project-1-2f463",
-  //   storageBucket: "upenn-project-1-2f463.appspot.com",
-  //   messagingSenderId: "43611725747"
-  // };
-  // firebase.initializeApp(config);
+// Initialize Firebase
+// var config = {
+//   apiKey: "AIzaSyAbF3nKAaCZqR4M16UtQapLikADKBnGsPs",
+//   authDomain: "upenn-project-1-2f463.firebaseapp.com",
+//   databaseURL: "https://upenn-project-1-2f463.firebaseio.com",
+//   projectId: "upenn-project-1-2f463",
+//   storageBucket: "upenn-project-1-2f463.appspot.com",
+//   messagingSenderId: "43611725747"
+// };
+// firebase.initializeApp(config);
+
+// search news api when player is typed in search bar
+$("#searchButton").on("click", function (event) {
+  event.preventDefault();
+  $("tbody").empty();
+  var input = $("#playerSearch").val();
+  newsApi(input);
+  $("#playerSearch").val("");
+});
 
 
-  $("#searchButton").on("click", function(event) {
-    event.preventDefault();
-    $("#news").text("");
-    var input = $("#playerSearch").val();
-    
-    var queryURL = "https://newsapi.org/v2/everything?q=" + input + '&apiKey=162726561a464b569d09046f2862e7f1';
-    
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function (response) {
-      
-      var result = response.articles;
-      
-      for (var i = 0; i < 10; i++) {
+$(".logos").on("click", function () {
+  $("tbody").empty();
+  var input = $(this).attr("data-name")
+  newsApi(input);
+});
 
-          // var titleDiv = $("<div>");
+// This function grabs news API after taking in a input
+function newsApi(input){
+  var queryURL = "https://newsapi.org/v2/everything?q=" + input + '&apiKey=162726561a464b569d09046f2862e7f1';
 
-          // var imgTag = $("<img>")
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
 
-          var trTag = $("<tr>");
+    var result = response.articles;
 
-          var rowTag = $("<th>").text(i+1);
+    for (var i = 0; i < 10; i++) {
+      var trTag = $("<tr>");
+      var rowTag = $("<th>").text(i + 1);
+      var titleTag = $("<td>").text(result[i].title);
+      var contentTag = $("<td>").text(result[i].content);
 
-          var titleTag = $("<td>").text(result[i].title);
-          var contentTag = $("<td>").text(result[i].content);
-          
-          titleTag.attr("href", result[i].url);
-          
-          // trTag.text(rowTag + titleTag + contentTag)
+      titleTag.attr("href", result[i].url);
 
-          $("tbody").append(trTag)
+      $("tbody").append(trTag)
+      $("tbody").append(rowTag)
+      $("tbody").append(titleTag)
+      $("tbody").append(contentTag)
+    }
+  });
+}
 
-          $("tbody").append(rowTag)
-          $("tbody").append(titleTag)
-          $("tbody").append(contentTag)
-          
+// hover effect over logos
+$(".logos").mouseover(function () {
+  $(this).css("transition", "ease 0s");
+  $(this).css("border-color", $(this).attr("data-color"));
+  $(this).mouseout(function () {
+    $(this).css("border-color", "white")
+  });
+});
 
-          // console.log(result[i].title)
-          // console.log(result[i].content)
-          
-      }
-
-          // console.log(queryURL)
-            
-    });//end ajax
-  });//end onClick
+// automatic NBA Teams scrolls
+setInterval(function(){
+  $(".logos").css("transition", "ease 10s")
+  $(".logos").css("transform", "translate(-770px, 0px)")
+}, 10000)
+setInterval(function(){
+  $(".logos").css("transition", "ease 10s")
+  $(".logos").css("transform", "translate(30px, 0px)")
+}, 20000)
