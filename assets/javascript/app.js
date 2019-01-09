de300de34d97019a6fbf4dfe2fbdfcc1347c3cc3
ron881212 <42901139+ron881212@users.noreply.github.com>
2019-01-09 14:55:52 -0500

    I added ticket master (#15)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,14 +6,20 @@ $("#searchButton").on("click", function (event) {
   $("tbody").empty();
   var input = $("#playerSearch").val();
   newsApi(input);
+  tmApi(input);
+  $(".ticketmaster").hide();
+  $(".newsroom").removeClass("col-9");
   $("#playerSearch").val("");
 });
 
-
-$(".logos").on("click", function () {
+// search news and ticket master api when team logo is clicked
+$(document).on("click", ".logos", function () {
   $("tbody").empty();
-  var input = $(this).attr("data-name")
+  var input = $(this).attr("data-name");
+  $(".newsroom").addClass("col-9");
+  $(".ticketmaster").show();
   newsApi(input);
+  tmApi(input);
 });
 
 // This function grabs news API after taking in a input
@@ -44,10 +50,33 @@ function newsApi(input){
       rowTag.append(imgTag);
       contentTag.append(content);
 
-      $("tbody").append(trTag)
-      $("tbody").append(rowTag)
-      $("tbody").append(titleTag)
-      $("tbody").append(contentTag)
+      $("#newsTable").append(trTag)
+      $("#newsTable").append(rowTag)
+      $("#newsTable").append(titleTag)
+      $("#newsTable").append(contentTag)
+    }
+  });
+}
+
+// This function grabs ticket info taking in a input
+function tmApi(input){
+  var apiKey = "fHVIkb41Pw4obhdgNbHPq4igfwZ5hAKZ"
+  var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=" + apiKey + "&keyword=" + input;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+
+    var result = response._embedded;
+    // console.log(response);
+    for (var i = 0; i < 10; i++) {
+      var trTag = $("<tr>");
+      var titleTag = $("<td>").text(result.events[i].name);
+      var ticketTag = $("<td>").html('<a target="_blank" href="'+ result.events[i].url +'"><i class="fas fa-5x fa-ticket-alt"></i></a>');
+      // console.log(result.events[i]);
+      trTag.append(titleTag, ticketTag);
+      $("#tmTable").append(trTag);
     }
   });
 }
