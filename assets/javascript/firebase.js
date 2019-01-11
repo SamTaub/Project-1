b138e12d5d7f5ea49397be1abdac184dce8a647b
ron881212 <ron881212@gmail.com>
2019-01-10 23:23:39 -0500

    added functionalty to chat

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -1,29 +1,44 @@
 //Initialize Firebase
 var config = {
-  apiKey: "AIzaSyAbF3nKAaCZqR4M16UtQapLikADKBnGsPs",
-  authDomain: "upenn-project-1-2f463.firebaseapp.com",
-  databaseURL: "https://upenn-project-1-2f463.firebaseio.com",
-  projectId: "upenn-project-1-2f463",
-  storageBucket: "upenn-project-1-2f463.appspot.com",
-  messagingSenderId: "43611725747"
-};
+    apiKey: "AIzaSyAltHOPXSSGBL2TCstCTDwakVwDwRMlC5A",
+    authDomain: "project-1-1e533.firebaseapp.com",
+    databaseURL: "https://project-1-1e533.firebaseio.com",
+    projectId: "project-1-1e533",
+    storageBucket: "project-1-1e533.appspot.com",
+    messagingSenderId: "877020008013"
+  };
 
 firebase.initializeApp(config);
 
 var database = firebase.database();
+var user = "anonymous";
 
-$('#comment').on("click", function(){
+$(document).on("click", "#login", function(){
+    user = $("#user-name").val();
+  
+    $("#user-name").val('');
+    console.log(user);
+    
+  })
+
+$('#comment').on("click", function(event){
 
     event.preventDefault();
     if($('#user-comment').val() === ""){
         return;
     } else {
     var comment = $('#user-comment').val().trim();
-
+    var date = new Date();
+    var mill = date.getTime(); 
+    // console.log(date);
+    
     var displayComment = {
-        comment: comment
+        comment: comment,
+        timestamp: mill,
+        username: user
     };
-
+    // console.log(timestamp);
+    
     database.ref().push(displayComment);
 
     $("#user-comment").val('');
@@ -32,27 +47,24 @@ $('#comment').on("click", function(){
 
 database.ref().on("value", function (snapshot) {
 
-    var sv = snapshot.val();
-    console.log(sv);
-    comment = sv.comment;
-
-    //var appendComment = $('#comment-display').text(comment);
-
-
-    // for (var property in sv){
-    //     if (sv.hasOwnProperty(property)){
-    //         console.log(property.val())
-    //         var comment = $('<span>' + property.comment + '</span>')
-    //      $("#comment-display").prepend(comment);
-    //     }
-    // };
+    // var sv = snapshot.val();
+    // console.log(sv);
+    // comment = sv.comment;
+    // timestamp = sv.timestamp;
+    // console.log(sv.timestamp);
+    
     $('#comment-display').empty();
     snapshot.forEach(function(childSnapshot) {
-        console.log(childSnapshot.val())
+        // console.log(childSnapshot.val())
         var comment = childSnapshot.val().comment;
-        var commentSpan = $('<p>' + 'anonymous: ' + comment + '</p>')
+        var timestamp = childSnapshot.val().timestamp;
+        var username = childSnapshot.val().username;
+        // console.log(timestamp);
+        // console.log(ago(timestamp));
+        // console.log(jQuery.timeago(timestamp));
+        
+        var commentSpan = $('<p>' + username + ': ' + comment + '  ' + '<small class="timeago">'+ ago(timestamp) +'</small>' +'</p>')
         $("#comment-display").prepend(commentSpan);
     })
-
 
 });
